beca8c2fab4b40627d6166e802a4a632f7e1e413
daicham <daicham@gmail.com>
2011-05-29 17:31:51 +0900

    プラグイン情報を修正

--- init.rb
+++ init.rb
@@ -5,18 +5,14 @@ require 'redmine'
 # libの下にあるhookの設定を取り込む
 require 'issue_templates_issues_hook'
 
-#
-# Redmine Plugin ハンズオン用サンプルプラグイン
-# 自由に改変、配布してかまいません。
-# authorも変えてもらってかまいません。
-#
+# Original Sources: http://www.r-labs.org/projects/r-labs/wiki/%E3%83%97%E3%83%A9%E3%82%B0%E3%82%A4%E3%83%B3%E3%83%8F%E3%83%B3%E3%82%BA%E3%82%AA%E3%83%B3_%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB
 Redmine::Plugin.register :redmine_issue_templates do
   name 'Redmine Issue Templates plugin'
-  author 'Haruyuki Iida'
+  author 'Daisuke Takeuchi'
   description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://twitter.com/haru_iida'
+  version '0.1.0'
+  url 'http://d.hatena.ne.jp/daicham/'
+  author_url 'http://twitter.com/daicham'
   requires_redmine :version_or_higher => '1.0.0'
 
   # 各アクションの権限定義。ここで定義した権限が管理メニューの「ロールと権限」に表示される。
